f16aae27c06c2278992c049cfac5dd949e217313
Kirill Lastovirya <kirill.lastovirya@moex.com>
2015-10-22 15:46:42 +0300

    added files to the repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to closure-sandbox
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/closure_sandbox/core_test.clj
@@ -0,0 +1,24 @@
+(ns closure-sandbox.core-test
+  (:require [clojure.test :refer :all]
+            [closure-sandbox.core :refer :all]))
+
+(deftest is-multiple-of-test
+  (testing "checking is-multiple"
+    (is (= true (is-multiple-of 3 3)))
+    (is (= true (is-multiple-of 6 3)))
+    (is (= false (is-multiple-of 7 3))))
+
+  (testing "checking is-multiple-both"
+    (is (= true (is-multiple-of-3-or-5 3)))
+    (is (= true (is-multiple-of-3-or-5 5)))
+    (is (= true (is-multiple-of-3-or-5 15)))
+    (is (= false (is-multiple-of-3-or-5 7)))
+    (is (= false (is-multiple-of-3-or-5 14))))
+
+  (testing "only-multiples-filter"
+    (is (= [3 5 6 9] (only-multiples-of-3-or-5 9)))
+    (is (= [] (only-multiples-of-3-or-5 2)))
+    (is (= [3] (only-multiples-of-3-or-5 3)))
+    (is (= [3 5] (only-multiples-of-3-or-5 5)))
+  )
+)
